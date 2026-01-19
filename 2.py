763bac6e5be405cff1c2efa2c0a9bbdb449eb1a4
ashutosh25137-web <ashutosh.25137@ee.du.ac.in>
2026-01-19 22:49:57 +0530

    2.py

---
+++ 2.py
@@ -0,0 +1,18 @@
+# Arranging Clothes in a Luggage Bag
+
+1.Confirm the duration of trip 7 days trip
+2. Select clothes according to daily uses and activities
+3. Separate clothes into categories
+        - Heavy Clothes 
+        - Light Clothes
+        - Delicate Clothes
+        - Accessories
+4. Folding all clothes to save space
+5. Place heavy clothes like jeans and jackets at the bottom of bag
+6. Place light clothes like t-shirt and shirts above them
+7. Keep delicate clothes in separate protective cover
+8. using side pocket of bag for belt, socks and accessories
+9. keep daily use clothes on top of clothes for easy access
+10. Filling empty space with daily and most important uses accessories like brush and small items
+11. checking all that clothes are placed safe and properly
+12. Close the bag finally after confirmation of all clothes..
